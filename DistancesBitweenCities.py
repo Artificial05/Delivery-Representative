24067b53b6f6b404ea22c18c720f3a168642e3e8
Artificial05 <devloptool@outlook.com>
2025-05-06 12:01:56 +0000

    Give latitude andlongtitude of specific place11

--- DistancesBitweenCities.py
+++ DistancesBitweenCities.py
@@ -1,5 +1,11 @@
 import pandas, os 
-os,listdir()
+os.listdir()
+
+if not os.path.exists("expanded_agents_data.csv"):
+    raise FileNotFoundError("Data file missing ")
+if not os.path.exists("expanded_orders_data.csv"):
+    raise FileNotFoundError("Data file missing ")
+df1= pandas.read_csv("expanded_orders_data.csv")
 df2= pandas.read_csv("expanded_agents_data.csv")
 dir (geopy)
 from geopy.geocoders import Nominatim
