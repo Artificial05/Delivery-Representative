e93028d2e598c6f45a3d96b3689d3a6493d882b7
Artificial05 <devloptool@outlook.com>
2025-05-06 11:48:30 +0000

    Give latitude andlongtitude of specific place5

--- DistancesBitweenCities.py
+++ DistancesBitweenCities.py
@@ -4,7 +4,7 @@ df1= pandas.read_csv("expanded_orders_data.csv")
 df2= pandas.read_csv("expanded_agents_data.csv")
 dir (geopy)
 from geopy.geocoders import Nominatim
-nom = Nominatim()
+nom = Nominatim(user_agent="DistancesBitweenCities")
 n = nom.geocode("London eye")
 print(n.latitude, n.longitude)
 type(n)
