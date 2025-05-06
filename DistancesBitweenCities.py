ef80e3341250bfd571c9c139602910493f3ff368
Artificial05 <devloptool@outlook.com>
2025-05-06 11:53:49 +0000

    Give latitude andlongtitude of specific place5

--- DistancesBitweenCities.py
+++ DistancesBitweenCities.py
@@ -1,6 +1,5 @@
 import pandas, os 
 os,listdir()
-df1= pandas.read_csv("expanded_orders_data.csv")
 df2= pandas.read_csv("expanded_agents_data.csv")
 dir (geopy)
 from geopy.geocoders import Nominatim
