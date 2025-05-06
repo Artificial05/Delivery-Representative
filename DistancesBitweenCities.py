856f77c9bb037f144452416cc583ebce660b37a9
devlop@outlook.com <devloptool@outlook.com>
2025-05-06 12:29:26 +0000

    clean the token

--- DistancesBitweenCities.py
+++ DistancesBitweenCities.py
@@ -1,12 +1,12 @@
 import pandas, os 
 os.listdir()
+df1= pandas.read_csv("expanded_orders_data.csv")
+df2= pandas.read_csv("expanded_agents_data.csv")
 
 if not os.path.exists("expanded_agents_data.csv"):
     raise FileNotFoundError("Data file missing ")
 if not os.path.exists("expanded_orders_data.csv"):
     raise FileNotFoundError("Data file missing ")
-df1= pandas.read_csv("expanded_orders_data.csv")
-df2= pandas.read_csv("expanded_agents_data.csv")
 dir (geopy)
 from geopy.geocoders import Nominatim
 nom = Nominatim(user_agent="DistancesBitweenCities")
