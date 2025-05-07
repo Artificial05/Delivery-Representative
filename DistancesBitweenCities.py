f0531d8e1487bffc4ff78218b94e6422dfca8925
devlop@outlook.com <devloptool@outlook.com>
2025-05-07 06:35:56 +0000

    find latitude and longtitude

--- DistancesBitweenCities.py
+++ DistancesBitweenCities.py
@@ -1,14 +1,16 @@
 import pandas, os 
-os.listdir()
-df1= pandas.read_csv("expanded_orders_data.csv")
-df2= pandas.read_csv("expanded_agents_data.csv")
-
-if not os.path.exists("expanded_agents_data.csv"):
-    raise FileNotFoundError("Data file missing ")
-if not os.path.exists("expanded_orders_data.csv"):
-    raise FileNotFoundError("Data file missing ")
-dir (geopy)
 from geopy.geocoders import Nominatim
+os.listdir()
+
+def load_csv(file_name): # mitigate the risk of FileNotFoundError
+    if not os.path.exists(file_name):
+        raise FileNotFoundError(f"Data file '{file_name}' missing")
+    return pd.read_csv(file_name)
+
+df2 = load_csv("expanded_agents_data.csv")
+df1 = load_csv("expanded_orders_data.csv")
+
+dir (geopy)
 nom = Nominatim(user_agent="DistancesBitweenCities")
 n = nom.geocode("London eye")
 print(n.latitude, n.longitude)
